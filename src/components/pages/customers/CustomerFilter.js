3e94ed430d04d863253f27eff718340db7f5faf5
Java0807 <uzjava0ff@gmail.com>
2025-04-05 17:18:06 +0500

    dashboard img

--- src/components/pages/customers/CustomerFilter.js
+++ src/components/pages/customers/CustomerFilter.js
@@ -6,7 +6,7 @@ function CustomerFilter() {
   return (
     <div>
       <div className={styles["status"]}>
-        <Image src="/allstatus.png" width={24} height={24} alt="z" />
+        <Image src="/icon_filter.png" width={24} height={24} alt="z" />
         <select>
           <option>Filter</option>
           <option>Name</option>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import MainLayout from "@/components/common/layouts/MainLayout";
 import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
+import CustomerFilter from "@/components/pages/customers/CustomerFilter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,86 @@ export default function Dashboard() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div style={{ display: router.asPath === "/" ? "block" : "none" }}>
-        <h1 style={{ marginLeft: "20px" }}>Java </h1>
-        <h1 style={{ marginLeft: "20px" }}>{[date, "-", month, "-", year]}</h1>
+        <div
+          style={{
+            margin: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <h1>Dashboard</h1>
+            <p style={{ color: "gray", marginTop: "11px" }}>
+              Hi, Samantha. Welcome back to Sedap Admin!
+            </p>
+          </div>
+          <div>
+            <CustomerFilter />
+          </div>
+        </div>
+        <div>
+          <Image
+            style={{ margin: "20px" }}
+            src="/card_total_order.svg"
+            width={337}
+            height={172}
+            alt="z"
+          />
+          <Image
+            style={{ margin: "20px" }}
+            src="/card_total_order.svg"
+            width={337}
+            height={172}
+            alt="z"
+          />
+          <Image
+            style={{ margin: "20px" }}
+            src="/card_total_order.svg"
+            width={337}
+            height={172}
+            alt="z"
+          />
+          <Image
+            style={{ margin: "20px" }}
+            src="/card_total_order.svg"
+            width={337}
+            height={172}
+            alt="z"
+          />
+        </div>
+        <div
+          style={{
+            margin: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image src="/pieChart.svg" width={718} height={327} alt="z" />
+          <Image src="/card_chart_order.svg" width={714} height={327} alt="z" />
+        </div>
+        <div
+          style={{
+            margin: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Image
+            src="/card_total_revenue.svg"
+            width={972}
+            height={430}
+            alt="z"
+          />
+          <Image
+            src="/card_customer_map.svg"
+            width={456}
+            height={430}
+            alt="z"
+          />
+        </div>
       </div>
     </>
   );
